3fc3b06378f51ac10083fd95936d59d413eceb4c
Ali <alimovmuhammadali01222008@gmail.com>
2025-09-30 22:43:55 +0500

    created basic user input getter

--- src/main.py
+++ src/main.py
@@ -1,3 +1,17 @@
+from matrix_generator import display, matrix_generator
+
+
+def get_int(context: str) -> int:
+    while True:
+        try:
+            temp = int(input(context))
+        except ValueError:
+            print("Input only numbers")
+            continue
+        break
+    return temp
+
+
 def main() -> None:
     pass
 
